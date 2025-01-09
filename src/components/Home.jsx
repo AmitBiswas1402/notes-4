b169aa7ccc12cb713f5c807eb8e1ba910a097fe7
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-01-09 20:20:06 +0530

    feat: implement paste creation and updating functionality in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,56 @@
-import { useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
-  const [title, setTitle] = useState('')
-  const [value, setValue] = useState('')
-  const [searchParamas, setsearchParamas] = useSearchParams()
-  const pasteId = searchParamas.get('pasteId')
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  const [searchParamas, setsearchParamas] = useSearchParams();
+  const pasteId = searchParamas.get("pasteId");
+  const dispatch = useDispatch()
+
+  function createMyPaste() {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId || Date.now().toString(36),
+      createdAt: new Date().toISOString(),
+    }
+
+    if (pasteId) {
+      //update
+      dispatch(updateToPastes(paste))      
+    } else {
+      //create
+      dispatch(addToPastes(paste))      
+    }
+
+    //after create or update
+    setTitle("");
+    setValue("");
+    setsearchParamas({})
+  }
 
   return (
-    <div className="flex flex-row gap-7">
-      <input 
-        className="p-2 rounded-2xl mt-2"
-        type="text"
-        placeholder="enter title text here"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
+    <div>
+      <div className="flex flex-row gap-7 place-content-between">
+        <input
+          className="p-1 rounded-2xl mt-2 w-[66%] pl-4"
+          type="text"
+          placeholder="enter title text here"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-      <button>
-        {
-          pasteId ? 'Update My Paste' : 'Create My Paste'
-        }
-      </button>
+        <button onClick={createMyPaste} className="p-2 rounded-2xl mt-2">
+          {pasteId ? "Update My Paste" : "Create My Paste"}
+        </button>
+      </div>
+
+      <div className="mt-8">
+        <textarea className="rounded-2xl mt-4 min-w-[500px] p-4" value={value} placeholder="enter content here" onChange={(e) => setValue(e.target.value)} />
+      </div>
     </div>
-
-  )
-}
-export default Home
+  );
+};
+export default Home;
